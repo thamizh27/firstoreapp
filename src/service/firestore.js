a8b24ae5e269b3c2df12907ecee039877411c5d9
Thamizharasan <cdkthamizh27@gmail.com>
2021-02-18 17:53:34 +0530

    Adding service files into service folder

---
+++ src/service/firestore.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+// import firestore from "firebase/firebase-firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBtd3MstUUoQ9LhUaLjhDoVx-zrhE_HGHU",
+  authDomain: "sample-app-ff186.firebaseapp.com",
+  projectId: "sample-app-ff186",
+  storageBucket: "sample-app-ff186.appspot.com",
+  messagingSenderId: "235221359596",
+  appId: "1:235221359596:web:66f121217b484a5f603ec9",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
